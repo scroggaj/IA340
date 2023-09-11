0f1afe1387eb0be49938b7b395976a66083f4ff2
scroggaj <143352364+scroggaj@users.noreply.github.com>
2023-09-11 16:36:52 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -11,3 +11,12 @@ select * from student
 insert into professor(p_email,p_name,office)
 values ('p1@jmu.edu','p1','o1'),
 	('p2@jmu.edu','p2','o2')
+
+-- 2.3
+insert into course(c_number,c_name,room,p_email)
+values 
+	('c1','postgresql','r1','p1@jmu.edu'),
+	('c2','mongodb','r2','p2@jmu.edu'),
+	('c3','twitter','r1','p1@jmu.edu');
+
+select * from course
