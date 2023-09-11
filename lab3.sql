6d8f68a4e2aaf752b4a63bea08c374ba94486f8c
scroggaj <143352364+scroggaj@users.noreply.github.com>
2023-09-11 16:42:13 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -20,3 +20,14 @@ values
 	('c3','twitter','r1','p1@jmu.edu');
 
 select * from course
+
+-- 2.4
+insert into enroll(s_email,c_number)
+values ('s1@jmu.edu','c1'),
+	('s2@jmu.edu','c1'),
+	('s3@jmu.edu', 'c1'),
+	('s4@jmu.edu','c2'),
+	('s2@jmu.edu','c3'),
+	('s3@jmu.edu','c3');
+	
+select * from enroll
