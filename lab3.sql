8844e248b8e1a163759787cd0b5bbc8959a06ce1
scroggaj <143352364+scroggaj@users.noreply.github.com>
2023-09-11 16:21:30 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,1 +1,8 @@
-
+-- 2.1
+-- 	insert into student(s_email,s_emails,major)
+-- values ('s1@jmu.edu','s1','IA'),
+-- 	('s2@jmu.edu','s2','IA'),
+-- 	('s3@jmu.edu', 's3','ISAT')
+-- 	('s4@jmu.edu','s4','ISAT')
+		
+select * from student
