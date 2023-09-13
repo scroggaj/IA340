51f34a166c86b71a0f3eda1fe2133b88eac70357
scroggaj <143352364+scroggaj@users.noreply.github.com>
2023-09-13 15:45:36 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -2,7 +2,7 @@
 -- 	insert into student(s_email,s_emails,major)
 -- values ('s1@jmu.edu','s1','IA'),
 -- 	('s2@jmu.edu','s2','IA'),
--- 	('s3@jmu.edu', 's3','ISAT')
+-- 	('s3@jmu.edu', 's3','ISAT'),
 -- 	('s4@jmu.edu','s4','ISAT')
 		
 select * from student
@@ -14,12 +14,9 @@ values ('p1@jmu.edu','p1','o1'),
 
 -- 2.3
 insert into course(c_number,c_name,room,p_email)
-values 
-	('c1','postgresql','r1','p1@jmu.edu'),
+values('c1','postgresql','r1','p1@jmu.edu'),
 	('c2','mongodb','r2','p2@jmu.edu'),
-	('c3','twitter','r1','p1@jmu.edu');
-
-select * from course
+	('c3','twitter','r1','p1@jmu.edu')
 
 -- 2.4
 insert into enroll(s_email,c_number)
@@ -28,6 +25,4 @@ values ('s1@jmu.edu','c1'),
 	('s3@jmu.edu', 'c1'),
 	('s4@jmu.edu','c2'),
 	('s2@jmu.edu','c3'),
-	('s3@jmu.edu','c3');
-	
-select * from enroll
+	('s3@jmu.edu','c3')
